692c3544b2aff6428586fe2ac1bdc4c608df160f
Jan <jan.hoppel@protonmail.com>
2021-04-11 16:00:31 +0200

    basic animation, shader + perlin noise

--- optimizn/src/components/Animation/index.js
+++ optimizn/src/components/Animation/index.js
@@ -1,21 +1,32 @@
 
-import { Suspense } from 'react'
+import { Suspense, useRef } from 'react'
 import Background from './Background'
-import { MeshDistortMaterial, Icosahedron } from '@react-three/drei'
-
+import {Shader} from './Shaders/shader'
+import {useFrame} from '@react-three/fiber'
 export default function Index(props){
+    const ref = useRef();
+    useFrame(({clock})=>{
+        console.log(ref.current.material)
+        ref.current.material.uniforms.uTime.value = clock.getElapsedTime();
+    })
     return (
         <>
         <Suspense fallback={null}>
-            <ambientLight />
-                <Icosahedron args={[1,4]}>
-                <MeshDistortMaterial
-                    attach="material"
-                    distort={1}
-                    speed={1}
-                    color="#f25042"
-                />
-                </Icosahedron>
+        <pointLight position={[-10, -10, -10]} intensity={1} />
+        <ambientLight intensity={0.4} />
+        <spotLight
+          castShadow
+          angle={0.3}
+          penumbra={1}
+          position={[0, 10, 20]}
+          intensity={5}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+        />
+            <mesh ref={ref}>
+                <icosahedronBufferGeometry attach="geometry" args={[1,6]} />
+                <shaderMaterial args={[Shader]} />
+            </mesh>
             <Background />
         </Suspense>
         
