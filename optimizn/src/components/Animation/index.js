1a861ee30bb94298b678c40b542e0267e5b974e4
Jan <jan.hoppel@protonmail.com>
2021-04-12 11:56:12 +0200

    text #1

--- optimizn/src/components/Animation/index.js
+++ optimizn/src/components/Animation/index.js
@@ -1,8 +1,9 @@
 
-import { Suspense, useRef } from 'react'
+import { useRef } from 'react'
 import Background from './Background'
 import {Shader} from './Shaders/shader'
 import {useFrame} from '@react-three/fiber'
+import { Text } from '@react-three/drei';
 export default function Index(props){
     const ref = useRef();
     useFrame(({clock})=>{
@@ -10,7 +11,6 @@ export default function Index(props){
     })
     return (
         <>
-        <Suspense fallback={null}>
         <pointLight position={[-10, -10, -10]} intensity={1} />
         <ambientLight intensity={0.4}/>
         <spotLight
@@ -22,12 +22,26 @@ export default function Index(props){
           shadow-mapSize-width={1024}
           shadow-mapSize-height={1024}
         />
-            <mesh ref={ref} castShadow receiveShadow >
-                <icosahedronBufferGeometry attach="geometry" args={[1,6]} />
-                <shaderMaterial args={[Shader]}/>
-            </mesh>
-        </Suspense>
-        
+        <mesh ref={ref} castShadow receiveShadow scale={0.5} position={[2.0,0.0,0.0]}>
+            <icosahedronBufferGeometry attach="geometry" args={[1,30]} />
+            <shaderMaterial args={[Shader]}/>
+        </mesh>
+        <group >
+            <Text 
+                position={[-2.0,0.0,0.0]}
+                color={'#000000'}
+                fontSize={0.8}
+                >
+                optimizn.
+            </Text>
+            <Text
+                position={[-1.5, -1.0, 0.0]}
+                color={'#000000'}
+                fontSize={0.4}
+                >
+                {'innovative and\ncreative development'}
+            </Text>
+        </group>
         </>
     )
 }
